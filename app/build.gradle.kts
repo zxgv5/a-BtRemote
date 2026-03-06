3b625bb0872d61bf5479d2a0d8b47d2212c771b0
Atharok <atharok@duck.com>
2026-03-06 16:29:23 +0100

    New release (1.10.0).

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -13,8 +13,8 @@ android {
         applicationId = "com.atharok.btremote"
         minSdk = 28
         targetSdk = 36
-        versionCode = 21
-        versionName = "1.9.1"
+        versionCode = 22
+        versionName = "1.10.0"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
         vectorDrawables {
